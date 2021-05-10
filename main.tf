f1783d7ca3b2fb97caaeb16bedd6b673b72582ea
Sagar Ravkhande <Sagar.Ravkhande@tomtom.com>
2021-05-10 17:08:43 +0530

    added files for tf cloud initially

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "shaggys-Cabana"
+
+    workspaces {
+      name = "tfc-guide-example"
+    }
+  }
+}
+
+provider "aws" {
+	region = "us-east-1"
+        access_key = var.access_key
+	secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "test" {
+	bucket_prefix = "test-"
+}
